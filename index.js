2118b673e77ddf89f494881de0f62d0448aa35f4
shejanNizam <nizam.shejan11@gmail.com>
2024-11-02 21:37:02 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require("express");
-const app = express();
-// require mongoDB in here
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
 require("dotenv").config();
+
 const port = process.env.PORT || 7000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://docUser:3wMjyHBkbvHHwOIP@cluster0.jjtu4en.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("CAR SERVER is running");
