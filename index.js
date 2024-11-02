682b3f55c320ec0ecc0ff83e9cc883fda36768f8
shejanNizam <nizam.shejan11@gmail.com>
2024-11-02 21:39:06 +0600

    modified user and pas

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://docUser:3wMjyHBkbvHHwOIP@cluster0.jjtu4en.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjtu4en.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
