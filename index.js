d72bc4c75efd65e3d671d932b2f14a5dbaec74fd
shejanNizam <nizam.shejan11@gmail.com>
2024-11-04 19:35:09 +0600

    create get booking data api

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function run() {
     // get some data with query ( Read Operation )
     app.get("/bookings", async (req, res) => {
       let query = {};
-      if (req.query.email) {
+      if (req.query?.email) {
         query = { email: req.query.email };
       }
       const result = await bookingCollection.find(query).toArray();
