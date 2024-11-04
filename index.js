adc1e3d404a9ecc1b68ed6fc93d076026ef4c3a0
shejanNizam <nizam.shejan11@gmail.com>
2024-11-04 19:47:36 +0600

    create single delete api

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete single data ( Delete Operation )
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
